79ee11cd044d2069327cf677f78299a6a0742f11
Harley <yu.h.lin1991@gmail.com>
2022-10-11 00:14:09 +0800

    finish sidebar, body & footer flex division

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,8 @@
 import React from "react";
+import "./Sidebar.css";
 
 function Sidebar() {
-  return <div>Sidebar</div>;
+  return <div className="sidebar">Sidebar</div>;
 }
 
 export default Sidebar;
