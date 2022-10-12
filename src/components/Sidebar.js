8e7574804cb20ff1123cc172f369a64c6b65678d
Harley <yu.h.lin1991@gmail.com>
2022-10-12 21:31:22 +0800

    create sidebar option component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./Sidebar.css";
 
+const image = require("./../assets/img/spotify-logo-white.png");
+
 function Sidebar() {
-  return <div className="sidebar">Sidebar</div>;
+  return (
+    <div className="sidebar">
+      <img className="sidebar-logo" src={image} alt="logo" />
+    </div>
+  );
 }
 
 export default Sidebar;
