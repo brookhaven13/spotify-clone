9c4ff217335cdaa64977fb202ea7f838cf3c93ad
Harley <yu.h.lin1991@gmail.com>
2022-10-13 23:59:24 +0800

    finish footer slider and buttons styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,28 +1,38 @@
 import React from "react";
 import "./Footer.css";
 import { Grid, Slider } from "@mui/material";
-import PlayCircleOutlineRoundedIcon from "@mui/icons-material/PlayCircleOutlineRounded";
+import PlayCircleFilledRoundedIcon from "@mui/icons-material/PlayCircleFilledRounded";
 import ShuffleRoundedIcon from "@mui/icons-material/ShuffleRounded";
 import SkipPreviousRoundedIcon from "@mui/icons-material/SkipPreviousRounded";
 import SkipNextRoundedIcon from "@mui/icons-material/SkipNextRounded";
 import RepeatRoundedIcon from "@mui/icons-material/RepeatRounded";
 import PlaylistPlayRoundedIcon from "@mui/icons-material/PlaylistPlayRounded";
 import VolumeDownRoundedIcon from "@mui/icons-material/VolumeDownRounded";
-import VolumeUpRoundedIcon from "@mui/icons-material/VolumeUpRounded";
 
 function Footer() {
   return (
     <div className="footer">
       <div className="footer-left">
-        <span>Album and song details</span>
+        <img
+          className="footer-album-photo"
+          src="https://upload.wikimedia.org/wikipedia/en/8/85/Adele_-_Hello_%28Official_Single_Cover%29.png"
+          alt="album_photo"
+        />
+
+        <div className="footer-song-info">
+          <h4>Hello</h4>
+          <p>Adele</p>
+        </div>
       </div>
+
       <div className="footer-center">
-        <ShuffleRoundedIcon />
-        <SkipPreviousRoundedIcon />
-        <PlayCircleOutlineRoundedIcon />
-        <SkipNextRoundedIcon />
-        <RepeatRoundedIcon />
+        <ShuffleRoundedIcon className="footer-icon" />
+        <SkipPreviousRoundedIcon className="footer-icon" />
+        <PlayCircleFilledRoundedIcon className="footer-icon large" />
+        <SkipNextRoundedIcon className="footer-icon" />
+        <RepeatRoundedIcon className="footer-icon" />
       </div>
+
       <div className="footer-right">
         <Grid container spacing={2}>
           <Grid item>
@@ -33,9 +43,6 @@ function Footer() {
           </Grid>
           <Grid item xs>
             <Slider />
-          </Grid>
-          <Grid item>
-            <VolumeUpRoundedIcon />
           </Grid>
         </Grid>
       </div>
