d6d95416678d7a9f5fe4bfae2ba757382fdd6b62
Harley <yu.h.lin1991@gmail.com>
2022-10-13 22:30:21 +0800

    add icons to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,14 @@
 import React from "react";
 import "./Footer.css";
+import { Grid, Slider } from "@mui/material";
+import PlayCircleOutlineRoundedIcon from "@mui/icons-material/PlayCircleOutlineRounded";
+import ShuffleRoundedIcon from "@mui/icons-material/ShuffleRounded";
+import SkipPreviousRoundedIcon from "@mui/icons-material/SkipPreviousRounded";
+import SkipNextRoundedIcon from "@mui/icons-material/SkipNextRounded";
+import RepeatRoundedIcon from "@mui/icons-material/RepeatRounded";
+import PlaylistPlayRoundedIcon from "@mui/icons-material/PlaylistPlayRounded";
+import VolumeDownRoundedIcon from "@mui/icons-material/VolumeDownRounded";
+import VolumeUpRoundedIcon from "@mui/icons-material/VolumeUpRounded";
 
 function Footer() {
   return (
@@ -8,10 +17,27 @@ function Footer() {
         <span>Album and song details</span>
       </div>
       <div className="footer-center">
-        <span>Player controls</span>
+        <ShuffleRoundedIcon />
+        <SkipPreviousRoundedIcon />
+        <PlayCircleOutlineRoundedIcon />
+        <SkipNextRoundedIcon />
+        <RepeatRoundedIcon />
       </div>
       <div className="footer-right">
-        <span>volumn controls</span>
+        <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlayRoundedIcon />
+          </Grid>
+          <Grid item>
+            <VolumeDownRoundedIcon />
+          </Grid>
+          <Grid item xs>
+            <Slider />
+          </Grid>
+          <Grid item>
+            <VolumeUpRoundedIcon />
+          </Grid>
+        </Grid>
       </div>
     </div>
   );
